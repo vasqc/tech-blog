d483167bcfe3276055cfb7ac9888b28a23ad79e1
vasqc <90007988+vasqc@users.noreply.github.com>
2022-03-16 12:07:17 -0400

    handlebars

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
